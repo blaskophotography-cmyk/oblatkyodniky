b5c373e877c0b8cd08edae7d964b10aff9ab1b6c
blaskophotography-cmyk <blaskophotography@gmail.com>
2026-09-09 18:20:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,170 @@ const sendOrder = document.getElementById("sendOrder");
 
 const SCRIPT_URL =
     "https://script.google.com/macros/s/AKfycbxh7g7B8eW97umJ7zZYTGAIW6UNC7vSro-KoTu8UNqSpYmBLMBfUFYG647OV9tRXLIA4w/exec";
-
+const products = [
+    {
+        category: "SLADKÉ",
+        type: "Sladké ploché",
+        pack: "20 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 3.00 },
+            { name: "Orechové", price: 4.00 },
+            { name: "Vanilkové", price: 3.50 },
+            { name: "Škoricové", price: 3.50 }
+        ]
+    },
+    {
+        category: "TRUBIČKY",
+        type: "Sladké trubičky",
+        pack: "15 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 3.00 },
+            { name: "Orechové", price: 4.00 },
+            { name: "Vanilkové", price: 3.50 },
+            { name: "Škoricové", price: 3.50 }
+        ]
+    },
+    {
+        category: "SLANÉ",
+        type: "Slané",
+        pack: "50 ks / bal",
+        items: [
+            { name: "Slané / Bez príchute", price: 2.50 },
+            { name: "Bryndzové", price: 3.50 },
+            { name: "Rascové", price: 3.00 },
+            { name: "Cesnakové", price: 3.00 }
+        ]
+    },
+    {
+        category: "BEZLAKTÓZOVÉ",
+        type: "Ploché",
+        pack: "20 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "BEZLAKTÓZOVÉ",
+        type: "Trubičky",
+        pack: "15 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "BEZLAKTÓZOVÉ",
+        type: "Slané",
+        pack: "50 ks / bal",
+        items: [
+            { name: "Slané / Bez príchute", price: 3.50 },
+            { name: "Cesnakové", price: 4.50 },
+            { name: "S rascou", price: 4.50 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ",
+        type: "Ploché",
+        pack: "20 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ",
+        type: "Trubičky",
+        pack: "15 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ",
+        type: "Slané",
+        pack: "50 ks / bal",
+        items: [
+            { name: "Slané / Bez príchute", price: 3.50 },
+            { name: "Cesnakové", price: 4.50 },
+            { name: "S rascou", price: 4.50 },
+            { name: "Bryndzové", price: 5.00 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ + BEZLAKTÓZOVÉ",
+        type: "Ploché",
+        pack: "20 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 5.00 },
+            { name: "Orechové", price: 6.00 },
+            { name: "Vanilkové", price: 5.50 },
+            { name: "Škoricové", price: 5.50 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ + BEZLAKTÓZOVÉ",
+        type: "Trubičky",
+        pack: "15 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 5.00 },
+            { name: "Orechové", price: 6.00 },
+            { name: "Vanilkové", price: 5.50 },
+            { name: "Škoricové", price: 5.50 }
+        ]
+    },
+    {
+        category: "BEZLEPKOVÉ + BEZLAKTÓZOVÉ",
+        type: "Slané",
+        pack: "50 ks / bal",
+        items: [
+            { name: "Slané / Bez príchute", price: 4.50 },
+            { name: "S rascou", price: 5.50 },
+            { name: "Cesnakové", price: 5.50 }
+        ]
+    },
+    {
+        category: "VEGÁNSKE",
+        type: "Ploché",
+        pack: "20 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "VEGÁNSKE",
+        type: "Trubičky",
+        pack: "15 ks / bal",
+        items: [
+            { name: "Sladké / Bez príchute", price: 4.00 },
+            { name: "Orechové", price: 5.00 },
+            { name: "Vanilkové", price: 4.50 },
+            { name: "Škoricové", price: 4.50 }
+        ]
+    },
+    {
+        category: "VEGÁNSKE",
+        type: "Slané",
+        pack: "50 ks / bal",
+        items: [
+            { name: "Slané / Bez príchute", price: 3.50 },
+            { name: "S rascou", price: 4.50 },
+            { name: "Cesnakové", price: 4.50 }
+        ]
+    }
+];
 let cart = [];
 
 
